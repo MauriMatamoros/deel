62f39fa27ab53918920fd90825a8602a76d6505d
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2024-01-25 17:15:19 -0600

    feat: :sparkles: GET /contracts users can now fetch their non terminated contracts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize } = require('./model')
+const { sequelize, Contract } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
 const { Op } = require('sequelize')
 const app = express()
@@ -23,4 +23,21 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
     if (!contract) return res.status(404).end()
     res.json(contract)
 })
+
+app.get('/contracts', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const contracts = await Contract.findAll({
+        where: {
+            status: {
+                [Op.ne]: 'terminated',
+            },
+            [Op.or]: [
+                { ContractorId: req.profile.id },
+                { ClientId: req.profile.id },
+            ],
+        },
+    })
+    res.json(contracts)
+})
+
 module.exports = app
