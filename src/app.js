09a7cfb756de314d7eb4913e160d9d690640bd61
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2024-01-26 12:55:38 -0600

    feat: :sparkles: POST /admin/best-clients

--- src/app.js
+++ src/app.js
@@ -198,7 +198,7 @@ app.get(
             FROM Profiles
             JOIN jobs_with_contracts ON ContractorId = Profiles.id
             WHERE jobs_with_contracts.paid = 1
-            AND jobs_with_contracts.paymentDate BETWEEN :startDate AND :endDate
+            AND jobs_with_contracts.paymentDate BETWEEN :startDate AND DATE(:endDate, '+1 day')
             GROUP BY profession
             ORDER BY total_earnings DESC
             LIMIT 1;
@@ -207,6 +207,59 @@ app.get(
         )
 
         res.send(row)
+    }
+)
+
+app.get(
+    '/admin/best-clients',
+    getProfile,
+    [
+        query('start')
+            .isISO8601()
+            .toDate()
+            .withMessage('start must be a valid date'),
+        query('end')
+            .isISO8601()
+            .toDate()
+            .withMessage('end must be a valid date'),
+        query('limit').optional().isNumeric(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { start, end } = req.query
+        const limit = req.query.limit || 2
+
+        const startDate = new Date(start).toISOString().slice(0, 10)
+        const endDate = new Date(end).toISOString().slice(0, 10)
+
+        const rows = await sequelize.query(
+            `
+                WITH jobs_with_contracts AS (
+                    SELECT *
+                    FROM Jobs
+                    JOIN Contracts
+                    ON Jobs.ContractId = Contracts.id
+                )
+
+                SELECT Profiles.id, firstName || ' ' || lastName AS fullName, jobs_with_contracts.price
+                FROM Profiles
+                JOIN jobs_with_contracts ON ClientId = Profiles.id
+                WHERE jobs_with_contracts.paid = 1
+                AND jobs_with_contracts.paymentDate BETWEEN :startDate AND DATE(:endDate, '+1 day')
+                GROUP BY Profiles.id
+                ORDER BY price DESC
+                LIMIT :limit;
+            `,
+            {
+                replacements: { startDate, endDate, limit },
+                type: QueryTypes.SELECT,
+            }
+        )
+
+        res.send(rows)
     }
 )
 
